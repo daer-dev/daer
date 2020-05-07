064d014128dda7dadde8425317e576c15e348237
d-herrero <daniel.herrero.101@gmail.com>
2020-05-07 03:18:05 +0200

    Home format fixed.

--- src/sections/hero/index.js
+++ src/sections/hero/index.js
@@ -5,93 +5,78 @@ import { Row, Col } from 'react-bootstrap'
 import Glitch from 'components/glitch'
 import Typewriter from 'typewriter-effect'
 import ThemeContext from '../../context'
+
 class Hero extends React.Component {
+  static contextType = ThemeContext
 
-    static contextType = ThemeContext
-
-    render() {
-        return ( <
-            section id = { `${this.props.id}` }
-            className = "hero"
-            style = {
-                { height: this.context.height } } >
-            <
-            Row >
-            <
-            Col md = { 6 }
-            className = "content" >
-            <
-            div className = "content-text" >
-            <
-            div className = "line-text" >
-            <
-            h4 > Hello, I 'm</h4> <
-            /div> <
-            Glitch text = "John Neo" / >
-            <
-            Typewriter options = {
+  render() {
+    return (
+      <section id={`${this.props.id}`} className="hero" style={ { height: this.context.height } }>
+        <Row>
+          <Col md={6} className="content">
+            <div className="content-text">
+              <div className="line-text">
+                <h4>Hello, I 'm</h4>
+              </div>
+              <Glitch text="Daniel Herrero" />
+              <Typewriter options = {
                 {
-                    strings: [
-                        'UI/UX Designer',
-                        'PHP Developer',
-                        'Mobile App Expert',
-                    ],
-                    autoStart: true,
-                    loop: true,
+                  strings: [
+                    '- Backend dev',
+                    '- IT Manager',
+                    '- Devops',
+                  ],
+                  autoStart: true,
+                  loop: true,
                 }
-            }
-            /> <
-            button className = "hover-button" >
-            <
-            span > Download CV < /span> <
-            /button> <
-            /div> { this.icons() } <
-            /Col> <
-            Col md = { 6 }
-            className = "img" >
-            <
-            img src = { this.props.mainImg.childImageSharp.fluid.src }
-            alt = "person" /
-            >
-            <
-            /Col> <
-            /Row> <
-            /section>
-        )
-    }
+              } />
+            </div>
+            { this.icons() }
+          </Col>
+          <Col md={6} className="img">
+            <img src={this.props.mainImg.childImageSharp.fluid.src} alt="person" />
+          </Col>
+        </Row>
+      </section>
+    )
+  }
 
-    icons() {
-        return this.props.icons.edges.map((value, index) => {
-            return ( <
-                img src = { value.node.childImageSharp.fluid.src }
-                className = { `animated fadeIn move-${
-            Math.floor(Math.random() * 10) % 2 === 0 ? 'up' : 'down'
-          } float-image` }
-                style = {
-                    {
-                        left: `${index * 10}%`,
-                        bottom: `${Math.random() *
-              (+(index % 2 === 0 ? 80 : 20) - +(index % 2 === 0 ? 70 : 10)) +
-              +(index % 2 === 0 ? 70 : 10)}%`,
-                    }
-                }
-                alt = "shape"
-                key = { index }
-                />
-            )
-        })
-    }
+  icons() {
+    return this.props.icons.edges.map((value, index) => {
+      return (
+        <img src={value.node.childImageSharp.fluid.src}
+          className={
+            `animated fadeIn move-${
+              Math.floor(Math.random() * 10) % 2 === 0 ? 'up' : 'down'
+            } float-image`
+          }
+          style={
+            {
+              left: `${index * 10}%`,
+              bottom: `${Math.random() *
+                (+(index % 2 === 0 ? 80 : 20) - +(index % 2 === 0 ? 70 : 10)) +
+                +(index % 2 === 0 ? 70 : 10)}%`
+            }
+          }
+          alt="shape"
+          key={index}
+        />
+      )
+    })
+  }
 }
 
-export default props => ( <
-        StaticQuery query = { graphql `
+export default props => (
+  <StaticQuery query={
+    graphql `
       query {
         icons: allFile(
           filter: {
             extension: { regex: "/(png)/" }
             relativeDirectory: { eq: "icons" }
           }
-        ) {
+        )
+        {
           edges {
             node {
               childImageSharp {
@@ -110,10 +95,12 @@ export default props => ( <
           }
         }
       }
-    ` }
-        render = {
-            ({ icons, Img }) => < Hero icons = { icons }
-            mainImg = { Img } {...props }
-            />} /
-            >
-        )
+    `
+  }
+  render = {
+    ({ icons, Img }) => <Hero icons={icons}
+    mainImg={Img} {...props}
+    />
+  }
+  />
+)
